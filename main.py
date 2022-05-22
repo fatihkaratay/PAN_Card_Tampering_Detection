abd156eada83f80ce0d2bc9c1ff5b19f72b160e3
fatih karatay <fatihkaratay@gmail.com>
2022-05-21 22:59:06 -0400

    convert these images to the grayscale format

--- main.py
+++ main.py
@@ -36,3 +36,7 @@ tampered.show()
 # load the two images to cv2
 original = cv2.imread('venv/pan_card_tampering/image/original.png')
 tampered = cv2.imread('venv/pan_card_tampering/image/tampered.png')
+
+# convert these images to the grayscale format
+original_gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+tampered_gray = cv2.cvtColor(tampered, cv2.COLOR_BGR2GRAY)
